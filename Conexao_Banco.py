2c12775e747756212341b742732c1428f4b1fe13
Lennon Lima <75624202+LennonLima100@users.noreply.github.com>
2021-01-21 11:20:53 -0300

    Create Conexao_Banco.py

---
+++ Conexao_Banco.py
@@ -0,0 +1,22 @@
+# Importação
+import pyodbc
+
+#Função de Retornar Conexão
+def retornar_conexao_sql():
+	server = 'NOME SERVIDOR'
+	database = 'NOME BANCO DE DADOS'
+	username = 'LOGIN'
+	password = 'SENHA'
+	driver = '{OBDC Driver 17 for SQL Server}'
+	string_conexao = 'Driver={SQL Server Native Client 11.0};Server='+server+';Database='+database+';UID='+username+';PWD='+ password
+    conexao = pyodbc.connect (string_conexao)
+    return conexao.cursor()
+    
+#Variável de Cursor
+cursor = retornar_conexao_sql()
+
+# Seleção de Teste
+cursor.execute("FAZER O SELECT DESEJADO")
+row = cursor.fetchone()
+if row:
+    print(row)
